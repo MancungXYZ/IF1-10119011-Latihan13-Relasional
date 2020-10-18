f57d66ab69556347d4bee1af55cafbef49e816a5
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-18 10:25:02 +0700

    IF1-10119011-Latihan13-Relasional

---
+++ src/if1/pkg10119011/latihan13/relasional/IF110119011Latihan13Relasional.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan13.relasional;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ */
+public class IF110119011Latihan13Relasional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bill = 10;
+        int bill2 = 20;
+        System.out.println("a == b = " + (bill == bill2));
+        System.out.println("a != b = " + (bill != bill2));
+        System.out.println("a > b = " + (bill > bill2));
+        System.out.println("a < b = " + (bill < bill2));
+        System.out.println("b >= a = " + (bill2 >= bill2));
+        System.out.println("b <= a = " + (bill2 <= bill2));
+    }
+    
+}
